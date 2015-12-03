6002f8183ed11dff8606ec932ad53b361dd753f4
pazzo83 <uvapazzo@gmail.com>
2015-12-03 18:00:40 -0500

    updating bootstrap
    curve ts holding all the data now

--- src/termstructures/bootstrap.jl
+++ src/termstructures/bootstrap.jl
@@ -10,120 +10,125 @@ type Discount <: BootstrapTrait end
 initial_value(::Discount) = 1.0
 max_iterations(::Discount) = 100
 
-function guess(::Discount, i::Int64, boot::Bootstrap)
-  if boot.validCurve
+function guess(::Discount, i::Int64, ts::TermStructure, valid::Boolean)
+  if valid
     # return previous iteration value
-    return boot.data[i]
+    return ts.data[i]
   end
-  
+
   if i == 2
     # first pillar
-    return 1.0 / (1.0 + avg_rate * boot.times[2])
+    return 1.0 / (1.0 + avg_rate * ts.times[2])
   end
-  
-  r = -log(boot.data[i - 1]) / boot.times[i - 1]
-  return exp(-r * boot.times[i])
+
+  r = -log(ts.data[i - 1]) / ts.times[i - 1]
+  return exp(-r * ts.times[i])
 end
 
-function min_value_after(::Discount, i::Int64, boot::Bootstrap)
-  if boot.validCurve
-    return boot.data[end] / 2.0
+function min_value_after(::Discount, i::Int64, ts::TermStructure, valid::Boolean)
+  if valid
+    return ts.data[end] / 2.0
   end
-  
-  dt = boot.times[i] - boot.times[i - 1]
-  return boot.data[i - 1] * exp( -max_rate * dt)
+
+  dt = ts.times[i] - ts.times[i - 1]
+  return ts.data[i - 1] * exp( -max_rate * dt)
 end
 
-function max_value_after(::Discount, i::Int64, boot::Bootstrap)
-  return boot.data[i - 1]
+function max_value_after(::Discount, i::Int64, ts::TermStructure)
+  return ts.data[i - 1]
 end
 
-function update_guess!(::Discount, i::Int64, boot::Boostrap, discount::Float64)
-  boot.data[i] = discount
-  return boot
+function update_guess!(::Discount, i::Int64, ts::TermStructure, discount::Float64)
+  ts.data[i] = discount
+  return ts
 end
 
 # BOOTSTRAPPING
 abstract Bootstrap
 
-type IterativeBootstrap <: Bootstrap
-  ts::TermStructure
-  times::Vector{Int64}
-  data::Vector{Float64}
-  errors::Vector{Function}
-  validCurve::Bool
-  
-  function IterativeBootstrap(ts::TermStructure)
-    # setup and initialize constructor
-    n = len(ts.instruments)
-    times = zeros(n)
-    errors = Vector{Function}(n)
-    data_initial = initial_value(ts.trait)
-    
-    # initialize data
-    if data_initial == 1.0
-      data = ones(n)
-    elseif data_initial == 0.0
-      data = zeros(n)
-    else
-      data = fill(data_initial, n)
-    end
-    
-    new(ts, times, data, false)
-  end
-end
+type IterativeBootstrap <: Bootstrap end
 
-function initialize!(boot::Bootstrap)
-  n = len(boot.times)
+# returns initial bootstrap state to Term Structure
+function initialize(::IterativeBootstrap, ts::TermStructure)
+  # get the intial data based on trait
+  data_initial = initial_value(ts.trait)
+
+  # initialize data
+  if data_initial == 1.0
+    ts.data = ones(n)
+  elseif data_initial == 0.0
+    ts.data = zeros(n)
+  else
+    ts.data = fill(data_initial, n)
+  end
+
+  # build times and error vectors (which have the functions for the solver)
   for i = 1:n
-    boot.times[i] = time_from_reference(boot.ts, boot.ts.instruments[i].maturityDate)
-    boot.errors[i] = bootstrap_error(boot, ts.instruments[i], i)
+    ts.times[i] = time_from_reference(ts, ts.instruments[i].maturityDate)
+    ts.errors[i] = bootstrap_error(ts, i)
   end
-  
-  return boot
 end
 
-function calculate!(boot::Bootstrap)
-  max_iter = max_iterations(boot.ts.trait)
-  
+function calculate!(::Bootstrap, ts::TermStructure, solver::Solver1D, first_solver::Solver1D)
+  max_iter = max_iterations(ts.trait)
+  prev_data = copy(ts.data) # need actual copy, not pointer, to check later for convergence
+  valid_data = ts.validCurve
+
   iterations = 0
   # if we get through this loop, we haven't converged
   while iterations < max_iter
-    for i = 2:len(boot.data)
-      
+    for i = 2:len(ts.data)
+
       # bracket root and calculate guess
-      min = min_value_after(boot.ts.trait, i, boot)
-      max = max_value_after(boot.ts.trait, i, boot)
-      guess = guess(boot.ts.trait, i, boot)
-      
+      min = min_value_after(ts.trait, i, ts, valid_data)
+      max = max_value_after(ts.trait, i, ts)
+      guess = guess(ts.trait, i, ts, valid_data)
+
       # adjust if needed
       if guess >= max
         guess = max - (max - min) / 5.0
       elseif (guess <= min)
         guess = min + (max - min) / 5.0
       end
-      
-      if !boot.validCurve
-        update!(boot.ts.interp, i)
+
+      if !valid_data
+        update!(ts.interp, i)
       end
-      
+
       # put this in a try / catch
-      if !boot.validCurve
-        # stuff
+      if !valid_data
+        # use first solver
+        root = solve(first_solver, ts.errors[i], ts.accuracy, guess, min, max)
+      else
+        root = solve(solver, ts.errors[i], ts.accuracy, guess, min, max)
       end
     end
+
+    # let's check for convergence
+    change = abs(data[2] - prev_data[2])
+    for i=3:len(ts.data)
+      change = max(change, abs(data[i] - prev_data[i]))
+    end
+    if change <= ts.accuracy
+      break # bye
+    end
+
+    valid_data = true
   end
+  ts.validCurve = true
+
+  return ts
 end
 
-function bootstrap_error(boot::Bootstrap, inst::Instrument, i::Int64)
+function bootstrap_error(ts::TermStructure, i::Int64)
   function bootstrap_error_inner(guess::Float64)
     # update trait
-    update_guess!(boot.ts.trait, i, boot, guess)
-    update!(boot.ts.interp, i)
-    return quote_error(boot.ts, inst)
+    update_guess!(ts.trait, i, ts, guess)
+    update!(ts.interp, i)
+    return quote_error(ts, ts.inst)
   end
-  
+
   return bootstrap_error_inner
 end
 
-quote_error(ts::TermStructure, inst::Instrument) = value(inst) - implied_quote(ts, inst)
+quote_error(ts::TermStructure, inst::Instrument) = value(inst) - implied_quote(inst, ts)
