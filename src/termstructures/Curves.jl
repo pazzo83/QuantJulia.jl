de2738daa38870cf927c356311e2720180fe4715
pazzo83 <uvapazzo@gmail.com>
2015-12-03 18:01:28 -0500

    Updating curves
    Curve ts now holds all data, and added outer constructor

--- src/termstructures/Curves.jl
+++ src/termstructures/Curves.jl
@@ -1,9 +1,39 @@
 # Curves
 
-abstract Curve
+abstract Curve <: TermStructure
 
-type PiecewiseLogCubicDiscountCurve <: Curve
-  calculation_date::Date
-  jump_dates::Vector{JumpDate}
+type PiecewiseYieldCurve <: Curve
+  reference_date::Date
+  instruments::Vector{Instrument}
   dc::DayCounter
+  interp::Interpolation
+  trait::BootstrapTrait
+  accuracy::Float64
+  boot::Bootstrap
+  times::Vector{Int64}
+  data::Vector{Float64}
+  errors::Vector{Function}
+  validCurve::Bool
+end
+
+function PiecewiseYieldCurve(reference_date::Date, instruments::Vector{Instrument}, dc::DayCounter, interp::Interpolation, trait::BootstrapTrait, accuracy::Float64, boot::Bootstrap)
+  # get the initial length of instruments
+  n = len(instruments)
+  # create an initial state of the curve
+  pyc = PiecewiseYieldCurve(reference_date,
+                            instruments,
+                            dc,
+                            interp,
+                            trait,
+                            accuracy,
+                            boot,
+                            Vector{Float64}(n),
+                            Vector{Float64}(n),
+                            Vector{Function}(n),
+                            false)
+
+  # initialize the bootstrapping
+  initialize(boot, pyc)
+
+  return pyc
 end
