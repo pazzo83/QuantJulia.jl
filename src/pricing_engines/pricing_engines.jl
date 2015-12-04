26c0aadf8b81610f4ef96022ab98c96a7f5175d6
pazzo83 <uvapazzo@gmail.com>
2015-12-03 19:08:56 -0500

    adding pricing engines

---
+++ src/pricing_engines/pricing_engines.jl
@@ -0,0 +1,20 @@
+# Pricing Engines module
+module PricingEngines
+
+using QuantJulia.TermStructures, QuantJulia.Instruments, QuantJulia.CF
+
+export PricingEngine, DiscountingBondEngine
+
+abstract PricingEngine
+type DiscountingBondEngine <: PricingEngine end
+
+function calculate(::DiscountingBondEngine, yts::YieldTermStructure, bond::Bond)
+  valuation_date = yts.reference_date
+  value = npv(bond.cashflows, yts, valuation_date, valuation_date)
+
+  # if the valuation_date is the same as the bond settlement date, then we don't have to recalculate
+
+  return value
+end
+
+end
