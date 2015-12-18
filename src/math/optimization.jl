c8e998eed30655bfa870131c0ab20cb5a23ea3a1
pazzo83 <uvapazzo@gmail.com>
2015-12-17 19:01:00 -0500

    Adding second simplex method from Optim

--- src/math/optimization.jl
+++ src/math/optimization.jl
@@ -181,6 +181,162 @@ function minimize!(simplex::Simplex, p::Problem, end_criteria::EndCriteria)
   end
 end
 
+centroid(p::Matrix) = reshape(mean(p, 2), size(p, 1))
+
+function dominates(x::Vector, y::Vector)
+  for i in 1:length(x)
+    @inbounds if x[i] <= y[i]
+      return false
+    end
+  end
+  return true
+end
+
+function dominates(x::Real, y::Vector)
+  for i in 1:length(y)
+    @inbounds if x <= y[i]
+      return false
+    end
+  end
+  return true
+end
+
+nmobjective(y::Vector, m::Integer, n::Integer) = sqrt(var(y) * (m / n))
+
+function minimize_2!(simplex::Simplex, prob::Problem, end_criteria::EndCriteria)
+  xtol = ftol = end_criteria.rootEpsilon
+  max_stationary_state_iterations = end_criteria.maxStationaryStateIterations
+  reset!(prob)
+  initial_x = prob.currentValue
+  iter_num = 0
+  m = length(initial_x)
+  initial_step = ones(length(initial_x))
+  a = 1.0
+  g = 2.0
+  b = 0.5
+
+  n = m + 1
+  p = repmat(initial_x, 1, n)
+  for i = 1:m
+    @inbounds p[i, i] += initial_step[i]
+  end
+
+  # count f'n calls
+
+  # Maintain a record of the value of f() at n points
+  y = Array(Float64, n)
+  for i = 1:n
+    @inbounds y[i] = value!(prob, p[:, i])
+  end
+
+  f_x_previous, f_x = NaN, nmobjective(y, m, n)
+
+  # Count iterations
+  iteration = 0
+
+  # Cache p_bar, y_bar, p_star, and p_star_star
+  p_bar = Array(Float64, m)
+  y_bar = Array(Float64, m)
+  p_star = Array(Float64, m)
+  p_star_star = Array(Float64, m)
+
+  # Iterate until convergence or exhaustion
+  f_converged = false
+
+  while !f_converged && iteration < end_criteria.maxIterations
+    # augment iter coutner
+    iteration += 1
+
+    # Find p_l and p_h, the min and max values of f() among p
+    y_l, l = findmin(y)
+    @inbounds p_l = p[:, l]
+    y_h, h = findmax(y)
+    @inbounds p_h = p[:, h]
+
+    # Compute the centroid of the non-maximal points
+    # also cache function values of all non-maximal points
+    fill!(p_bar, 0.0)
+    tmpindex = 0
+    for i = 1:n
+      if i != h
+        tmpindex += 1
+        @inbounds y_bar[tmpindex] = y[i]
+        @inbounds p_bar[:] += p[:, i]
+      end
+    end
+
+    for j = 1:m
+      @inbounds p_bar[j] /= m
+    end
+
+    # Compute a reflection
+    for j = 1:m
+      @inbounds p_star[j] = (1 + a) * p_bar[j] - a * p_h[j]
+    end
+    y_star = value!(prob, p_star)
+
+    if y_star < y_l
+      # Compute an expansion
+      for j = 1:m
+        @inbounds p_star_star[j] = g * p_star[j] + (1 - g) * p_bar[j]
+      end
+      y_star_star = value!(prob, p_star_star)
+
+      if y_star_star < y_l
+        @inbounds p_h[:] = p_star_star
+        @inbounds p[:, h] = p_star_star
+        @inbounds y[h] = y_star_star
+      else
+        p_h = p_star
+        @inbounds p[:, h] = p_star
+        @inbounds y[h] = y_star
+      end
+    else
+      if dominates(y_star, y_bar)
+        if y_star < y_h
+          @inbounds p_h[:] = p_star
+          @inbounds p[:, h] = p_h
+          @inbounds y[h] = y_star
+        end
+
+        # Compute a contraction
+        for j = 1:m
+          @inbounds p_star_star[j] = b * p_h[j] + (1 - b) * p_bar[j]
+        end
+        y_star_star = value!(prob, p_star_star)
+
+        if y_star_star > y_h
+          for i = 1:n
+            for j = 1:m
+              @inbounds p[j, i] = (p[j, i] + p_l[j]) / 2.0
+            end
+            @inbounds y[i] = value!(prob, p[:, i])
+          end
+        else
+          @inbounds p_h[:] = p_star_star
+          @inbounds p[:, h] = p_h
+          @inbounds y[h] = y_star_star
+        end
+      else
+        @inbounds p_h[:] = p_star
+        @inbounds p[:, h] = p_h
+        @inbounds y[h] = y_star
+      end
+    end
+
+    f_x_previous, f_x = f_x, nmobjective(y, m, n)
+
+    if f_x <= ftol
+      f_converged = true
+    end
+  end
+
+  minimum = centroid(p)
+  prob.functionValue = Float64(value!(prob, minimum))
+  prob.currentValue = minimum
+
+  return prob
+end
 
 ## Problem methods ##
 function reset!(p::Problem)
